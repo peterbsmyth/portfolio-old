8ae0fc8c7420a5d728d40a67aef5b89b91a55fca
Peter Smith <peterbutlersmith@me.com>
2015-02-12 14:29:50 -0500

    added wireframes docs, main.css, image placeholders

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Peter B Smith | Project Manager</title>
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<title>Peter B Smith Project Manager</title>
 	</head>
 	<body>
 		<header>
-			<h1>Peter B Smith | Project Manager</h1>
+			<h1>Peter B Smith</h1>
+			<h2>Project Manager</h2>
 			<nav>
 				<ul>
 					<li>PORTFOLIO</li>
@@ -18,7 +21,15 @@
 			</nav>
 		</header>
 		<section>
-
+			<h3>2015</h3>
+			<h4>KChat+MBTI</h4>
+			<img src="img/portfolio-01.png" alt="KChat+MBTI Screenshot">
+			<h3>2013</h3>
+			<h4>Bear Games</h4>
+			<img src="img/portfolio-03-temp.png" alt="Bear Games Screenshot">
+			<h3>2012</h3>
+			<h4>Island Dogs</h4>
+			<img src="img/portfolio-02-temp.png" alt="Island Dogs Screenshot">
 		</section>
 		<footer>
 			<h6>Designed + Developed by Peter B Smith &copy; 2015</h6>
