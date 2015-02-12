3226b384de0576f149b4857ec7774338325f3a2d
Peter Smith <peterbutlersmith@me.com>
2015-02-12 12:53:11 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Peter B Smith | Project Manager</title>
+	</head>
+	<body>
+		<header>
+			<h1>Peter B Smith | Project Manager</h1>
+			<nav>
+				<ul>
+					<li>PORTFOLIO</li>
+					<li>ABOUT</li>
+					<li>RESUMÉ</li>
+					<li>GitHub</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+
+		</section>
+		<footer>
+			<h6>Designed + Developed by Peter B Smith &copy; 2015</h6>
+		</footer>
+	</body>
+</html>
