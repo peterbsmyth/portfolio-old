822eb3346afe7e57ae4221bef6cb86a433315704
Peter Smith <peterbutlersmith@me.com>
2015-02-12 23:14:19 -0500

    organized wireframes, added about.php, updated index.php and main.css towards completion of mobile formatting

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<title>Peter B Smith Project Manager</title>
+	</head>
+	<body>
+		<header>
+			<h1>Peter B Smith</h1>
+			<h2>Project Manager</h2>
+			<nav>
+				<ul>
+					<li>PORTFOLIO</li>
+					<li>ABOUT</li>
+					<li>RESUMÉ</li>
+					<li>GitHub</li>
+				</ul>
+			</nav>
+		</header>
+		<div class="wrapper">
+			<section>
+				The best time to plant a tree was 20 years ago. The second-best time is now. - Chinese Proverb
+			</section>
+			<footer>
+				<h6 class="copyright">Designed + Developed by Peter B Smith &copy; 2015</h6>
+			</footer>
+		</div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -20,19 +20,43 @@
 				</ul>
 			</nav>
 		</header>
-		<section>
-			<h3>2015</h3>
-			<h4>KChat+MBTI</h4>
-			<img src="img/portfolio-01.png" alt="KChat+MBTI Screenshot">
-			<h3>2013</h3>
-			<h4>Bear Games</h4>
-			<img src="img/portfolio-03-temp.png" alt="Bear Games Screenshot">
-			<h3>2012</h3>
-			<h4>Island Dogs</h4>
-			<img src="img/portfolio-02-temp.png" alt="Island Dogs Screenshot">
-		</section>
-		<footer>
-			<h6>Designed + Developed by Peter B Smith &copy; 2015</h6>
-		</footer>
+		<div class="wrapper">
+			<section>
+				<ul class="portfolio">
+					<li>
+						<p class="year top-year">2015</p>
+						<ul class="project-list">
+						<a href="http://www.kchatmbti.com">
+							<li class="project">
+								<p>KChat+MBTI</p>
+								<img src="img/portfolio-01.png" alt="KCHAT+MBTI Screenshot">
+							</li>
+						</a>
+						</ul>
+					</li>
+					<li>
+						<p class="year">2013</p>
+						<ul class="project-list">
+							<li class="project">
+								<p>Bear Games</p>
+								<img src="img/portfolio-03-temp.png" alt="Bear Games Screenshot">
+							</li>
+						</ul>
+					</li>
+					<li>
+						<p class="year">2012</p>
+						<ul class="project-list">
+							<li class="project">
+								<p>Island Dogs</p>
+								<img src="img/portfolio-02-temp.png" alt="Island Dogs Screenshot">
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</section>
+			<footer>
+				<h6 class="copyright">Designed + Developed by Peter B Smith &copy; 2015</h6>
+			</footer>
+		</div>
 	</body>
 </html>
